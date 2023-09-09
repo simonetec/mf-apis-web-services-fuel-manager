5fea02c93cea7aa4942a9ad79de473cdb7c6aa28
simonetec <81451748+simonetec@users.noreply.github.com>
2023-09-09 14:36:04 -0300

    Criação e configuração dos modelos de veículo e consumo

---
+++ mf-apis-web-services-fuel-manager/NovaPasta1/Consumo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.NovaPasta1
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descrição { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Required]
+        public int VeiculoId { get; set; }
+        public Veiculo Veiculo  { get; set; }
+
+    }
+    public enum TipoCombustivel
+    {
+        Diesel,
+        Etanol,
+
+    }
+}
---
+++ mf-apis-web-services-fuel-manager/NovaPasta1/Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.NovaPasta1
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Marca { get; set; }
+        [Required]
+        public int Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
